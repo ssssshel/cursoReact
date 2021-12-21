bc2e9eb39655ac6c3626e483903f1116438863b4
lmnjcx <81728420+ssssshel@users.noreply.github.com>
2021-12-21 15:32:52 -0500

    jsx y propagacion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,15 @@
+import React from 'react'
+import Contador from './components/Contador'
+import Jsx from './components/Jsx'
+import Lista from './components/Lista'
 
 function App() {
   return (
     <div className="App">
       <h1>Hola mundo React</h1>
+      <Contador/>
+      <Jsx/>
+      <Lista/>
     </div>
   );
 }
---
+++ my-app/src/components/Jsx.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+
+const Jsx = () => {
+
+  const Temperatura = 21;
+
+  return ( 
+    <Fragment>
+      <h2>Frío o Calor</h2>
+      <h4>
+        {
+          Temperatura > 20 ? 'Calor' : 'Frío'
+        }
+      </h4>
+    </Fragment>
+  );
+}
+ 
+export default Jsx;
